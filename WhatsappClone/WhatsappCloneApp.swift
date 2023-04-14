4770a2dbba0a4a673ed39335938ae32964d9eac9
Furkan Yaman <111470818+yamanf@users.noreply.github.com>
2023-04-14 15:56:37 +0300

    Project created.

---
+++ WhatsappClone/WhatsappCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappCloneApp.swift
+//  WhatsappClone
+//
+//  Created by Furkan Yaman on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
